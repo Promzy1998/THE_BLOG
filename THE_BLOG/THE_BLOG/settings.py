14e686c2e2fdca8b5454c0b29bddba153ab2f8ff
Promzy1998 <Promoqwertyu@gmail.com>
2025-09-19 00:49:12 +0100

    enviroment created with requirement txt created for production

--- THE_BLOG/THE_BLOG/settings.py
+++ THE_BLOG/THE_BLOG/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(4e6k+c6tut&1c4l2fg4bw5uinkdr*rj6o0r2gqtff&7#4qgm'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -88,7 +89,7 @@ DATABASES = {
         'HOST':'localhost'
     }
 }
-DATABASES["default"]=dj_database_url.parse("postgresql://the_blog_fpnu_user:eS15hrtE78j9rdliUNJBv52rX3eDWPSG@dpg-d368nqffte5s73b8gii0-a.oregon-postgres.render.com/the_blog_fpnu")
+DATABASES["default"]=dj_database_url.parse(config('DATABASE_URL'))
 LOGIN_REDIRECT_URL="Index"
 
 # Password validation
