1455dd93e0e37b40768a4776f4a510b477afeb7e
Promzy1998 <Promoqwertyu@gmail.com>
2025-09-19 22:20:54 +0100

    Worked on settings and wsgi

--- THE_BLOG/THE_BLOG/wsgi.py
+++ THE_BLOG/THE_BLOG/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'THE_BLOG.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'THE_BLOG.THE_BLOG.settings')
 
 application = get_wsgi_application()
