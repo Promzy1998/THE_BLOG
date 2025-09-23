b98960a607bd19479f550ba16ea1a3b448b608f4
Promzy1998 <Promoqwertyu@gmail.com>
2025-09-23 14:57:23 +0100

    whitenoise install

--- THE_BLOG/THE_BLOG/settings.py
+++ THE_BLOG/THE_BLOG/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'assets')
 # media - where database images is stored
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
